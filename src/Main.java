a38471756ab7f387effbbaac93ef8bc6493481b2
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 20:17:02 -0300

    Para ler vários dados na mesma linha

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,18 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		char x; 
+		String x;
+		int y;
+		double z;
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
 		
-		x = sc.next().charAt(0);
-		
-		System.out.println("Você digitou: " + x);
+
+		System.out.println("dados digitados");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
 
 		sc.close();
 	}
