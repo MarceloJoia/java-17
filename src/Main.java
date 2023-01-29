6c41bfd25f4ed033cc10b26934b9d6f04e060d34
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 20:00:09 -0300

    Area do Trapézio

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,14 @@ import java.util.Locale;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		Locale.setDefault(Locale.US);
-
-		String product1 = "Computer";
-		String product2 = "Office desk";
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
 		
-		System.out.printf("Products: \nComputer, which price is $ %.2f%n", price1);
-		System.out.printf("%s, which price is $ 650,50%n", product2);
-		System.err.println();
-		System.out.printf("Record: %d years old, code %d and gender: %s", age, code, gender);
-		System.err.println();
-		
-	    System.out.println("Measue with eight decimal places: " + measure);
-	    System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-	    System.out.printf("US decimal point: %.2f%n", measure);
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		area = (b + B) / 2.0 * h;
+		System.out.println(area);
 		
 	}
 }
