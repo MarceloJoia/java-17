438294174b9799c92252a07645bc674f4c88f97a
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 20:01:38 -0300

    Variavel com ponto flutuante

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-		double b, B, h, area;
-		b = 6.0;
-		B = 8.0;
-		h = 5.0;
-		area = (b + B) / 2.0 * h;
+		float b, B, h, area;
+		b = 6f;
+		B = 8f;
+		h = 5f;
+		area = (b + B) / 2f * h;
 		System.out.println(area);
 		
 	}
