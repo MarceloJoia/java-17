a2a9271e0db7f442481a0430c9a79f72abf34ddd
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 19:54:28 -0300

    Exercício simples

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,24 @@ public class Main {
 		
 		Locale.setDefault(Locale.US);
 
-		/**
-		 * %f = ponto flutuante
-		 * %d = inteiro
-		 * %s = texto
-		 * %n = quebra de linha
-		 */
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
-		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
-
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.printf("Products: \nComputer, which price is $ %.2f%n", price1);
+		System.out.printf("%s, which price is $ 650,50%n", product2);
+		System.err.println();
+		System.out.printf("Record: %d years old, code %d and gender: %s", age, code, gender);
+		System.err.println();
+		
+	    System.out.println("Measue with eight decimal places: " + measure);
+	    System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+	    System.out.printf("US decimal point: %.2f%n", measure);
+		
 	}
-
 }
