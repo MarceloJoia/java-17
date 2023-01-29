386d2651e3bb18752daadd02a92c377e718da2dd
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 20:05:12 -0300

    Casting INT

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-		int a, b;
-		double resultado;
-		a = 5;
-		b = 2;
-		resultado = a / b;
-		System.out.println(resultado);
+		double a;
+		int b;
+		a = 5.0;
+		b = (int)a;
+		System.out.println(b);
 		
 		
 	}
