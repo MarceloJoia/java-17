cb33659a14ed73e0544f88debfb67a28628f18e1
Marcelo Joia <sitejoia@hotmail.com>
2023-01-29 19:34:58 -0300

    concatenar vários elementos

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ public class Main {
 		
 		Locale.setDefault(Locale.US);
 
-		double x = 10.35784;
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		
-		System.out.printf("RESULTADO = %.2f metros%n", x);
+		/**
+		 * %f = ponto flutuante
+		 * %d = inteiro
+		 * %s = texto
+		 * %n = quebra de linha
+		 */
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
 	}
 
