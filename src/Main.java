a6dd08a6bb4244929ebabf7e88abf4156ae70923
Marcelo Joia <sitejoia@hotmail.com>
2023-01-30 08:58:50 -0300

    split(...)

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,18 @@ public class Main {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		String original = " abcde FGHIJ ABC abc DEFG    ";
 		
-		// CAIXA BAIXA
-		String s01 = original.toLowerCase();
-		String s02 = original.toUpperCase();
-		String s03 = original.trim();
-		String s04 = original.substring(2);
-		String s05 = original.substring(2, 9);
-		String s06 = original.replace('a', 'x');
-		String s07 = original.replace("abc", "xy");
-		int i = original.indexOf("bc");
-		int j = original.lastIndexOf("bc");
+		String s = "potato apple lemon";
+		String[] vect = s.split(" ");
+		String word1 = vect[0];
+		String word2 = vect[1];
+		String word3 = vect[2];
 		
-		
-		System.out.println("original --> *" + original+"*");
-		System.out.println("toLowerCase --> *" + s01+"*");
-		System.out.println("toUpperCase --> *" + s02+"*");
-		System.out.println("trim --> *" + s03+"*");
-		System.out.println("substring(2) --> *" + s04+"*");
-		System.out.println("substring(2, 9) --> *" + s05+"*");
-		System.out.println("replace('a', 'x') --> *" + s06+"*");
-		System.out.println("replace(\"abc\", \"xy\") --> *" + s07 + "*");
-		System.out.println("indexOf(\"bc\")(primeira posição) --> *" + i + "*");
-		System.out.println("lastIndexOf(\"bc\")(última posição) --> *" + j + "*");
+		System.out.print("word1, word2 e word3: ");
+		System.out.println(word1 + ", " +word2 + ", "+ word3+"\n\r");
+		System.out.println("vect[0]: "+vect[0]);
+		System.out.println("vect[1]: "+vect[1]);
+		System.out.println("vect[2]: "+vect[2]);
 		
 		sc.close();
 	}
